94f663fb69fa8cc80ef2f1e9b2ff984ff7b07810
Todd Sparks <todd@drycanyon.com>
2016-11-28 15:10:15 -0700

    Saving the rest of the entries

--- file2.js
+++ file2.js
@@ -1,4 +1,5 @@
 var journal = [
-  "Today is the first day ever. For coding. Whatever"
- "I thought I would add another thing here."
- "I guess I'll keep adding some more."
+  "Today is the first day ever. For coding. Whatever",
+ "I thought I would add another thing here.",
+ "I guess I'll keep adding some more.",
+]
