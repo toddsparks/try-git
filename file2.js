4787eb043af8bbe2a8238cab48b7b8b809715020
Todd Sparks <todd@drycanyon.com>
2016-11-28 15:11:08 -0700

    Getting more practice

--- file2.js
+++ file2.js
@@ -2,4 +2,5 @@ var journal = [
   "Today is the first day ever. For coding. Whatever",
  "I thought I would add another thing here.",
  "I guess I'll keep adding some more.",
+ "I think maybe I'll get a little practice"
 ]
